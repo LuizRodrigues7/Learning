1d1c247a73e4fef17929d985810ddce0c7cde81d
LuizRodrigues7 <luiz.rsneto7@gmail.com>
2025-04-07 19:12:28 -0300

    att

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,21 @@
-let idade = 17;
+let usdInput = document.querySelector("#US$");
+let brlInput = document.querySelector("#BRL$");
+let img = document.querySelector("#img");
 
-if (idade >= 18){
-    console.log("Você é maior de idade");
-} else {
-    console.log("Você é menor de idade");
-}
+usdInput.addEventListener("keyup", function() {
+
+})
+
+brlInput.addEventListener("keyup", function() {
+
+})
+
+img.addEventListener("mouseover", function() {
+    img.style.transform = "scale(1.2)";
+    img.style.transition = "transform 0.5s ease";
+})
+
+img.addEventListener("mouseout", function() {
+    img.style.transform = "scale(1)";
+    img.style.transition = "transform 0.5s ease";
+})
