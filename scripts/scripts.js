b36156f7903f1e03187b269ccbec0424f45eb509
LuizRodrigues7 <luiz.rsneto7@gmail.com>
2025-05-30 14:50:10 -0300

    att

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,32 +7,32 @@ document.addEventListener("DOMContentLoaded", function () {
     sidebarBtn.addEventListener("click", function () {
         sidebar.classList.toggle("active");
         sidebarBtn.classList.toggle("active");
-    if (sidebar.style.left === "0px") {
-      sidebar.style.left = "-1000px";
-    } else {
-      sidebar.style.left = "0px";
-    }
-  });
-
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const id = entry.target.getAttribute("id");
-
-          navLinks.forEach((link) => {
-            link.classList.remove("active");
-            if (link.getAttribute("href") === `#${id}`) {
-              link.classList.add("active");
-            }
-          });
+        if (sidebar.style.left === "0px") {
+            sidebar.style.left = "-1000px";
+        } else {
+            sidebar.style.left = "0px";
         }
-      });
-    },
-    { threshold: 0.2 }
-  );
+    });
 
-  sections.forEach((section) => {
-    observer.observe(section);
-  });
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    const id = entry.target.getAttribute("id");
+
+                    navLinks.forEach((link) => {
+                        link.classList.remove("active");
+                        if (link.getAttribute("href") === `#${id}`) {
+                            link.classList.add("active");
+                        }
+                    });
+                }
+            });
+        },
+        { threshold: 0.2 }
+    );
+
+    sections.forEach((section) => {
+        observer.observe(section);
+    });
 });
